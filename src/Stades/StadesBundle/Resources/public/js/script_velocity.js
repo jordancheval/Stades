f002c561a1d9028f5d9f06606ea177b51243a4e3
RedSnowP <redsnowp57@gmail.com>
2015-12-07 22:42:04 +0100

    Update files - 07/12/2015
    Update files - 07/12/2015 - Composer update. Hammer.js added for
    gestures (TODO: update SCSS and CSS).

--- src/Stades/StadesBundle/Resources/public/js/script_velocity.js
+++ src/Stades/StadesBundle/Resources/public/js/script_velocity.js
@@ -9,6 +9,7 @@ var menu = document.getElementById("menu"),
     //toTop = document.getElementById(".to-top"),
     toggleDescription = document.getElementById("toggle-description"),
     description = document.getElementById("content-description"),
+    header = document.getElementById("header"),
     btnMenu = document.getElementById('btn-menu'),
     btnSearch = document.getElementById('btn-search'),
     searchInput = document.getElementById("stade_search"),
@@ -76,6 +77,23 @@ function hideMenuOnly(){
     }
 }
 
+/*
+ * hideMenuOnly
+ * 
+ * Permet seulement de montrer le menu
+ */
+function showMenuOnly(){
+    if(!menu.classList.contains("actif")){
+        V(menu, {marginLeft: 0}, speed);
+        menu.classList.add("actif");
+        btnMenu.classList.add("menu-actif");
+        //toTop.fadeOut(speed);
+        V(filter, { opacity: "0.4" }, { display: "block" }, speed);
+        content.classList.add("flou");
+        hideDescriptionOnly();
+    }
+}
+
 /**
  * Fonction pour cache ou montrer la description
  */
@@ -102,6 +120,20 @@ function hideDescriptionOnly(){
             V(description, "slideUp", speed);
             description.classList.remove("actif");
             toggleDescription.classList.remove("description-toggle-actif");
+        }
+    }
+}
+
+/**
+ * Fonction pour montrer la description seulement
+ */
+function showDescriptionOnly(){
+    if (toggleDescription !== null) {
+        if(!description.classList.contains("actif")){
+            V(description, "slideDown", speed);
+            description.classList.add("actif");
+            toggleDescription.classList.add("description-toggle-actif");
+            hideMenuOnly();
         }
     }
 }
@@ -153,6 +185,39 @@ if (toggleDescription !== null) {
     toggleDescription.addEventListener('click', hideShowDescription);
 }
 window.addEventListener("scroll", hideMenuOnly);
+
+/*----- With Hammer.js -----*/
+var filterHammer = new Hammer(filter);
+var menuHammer = new Hammer(menu);
+var contentHammer = new Hammer(content);
+
+filterHammer.on("panleft", function(ev){ hideMenuOnly(); });
+menuHammer.on("panleft", function(ev){ hideMenuOnly(); });
+
+/*var descriptionHammer = new Hammer(description);
+
+descriptionHammer.on("panup", function(ev){
+    console.log("GESTURE left !");
+    hideDescriptionOnly();
+});*/
+
+contentHammer.get('pan').set({ direction: Hammer.DIRECTION_ALL, threshold: 30 });
+
+contentHammer.on("panright", function(ev) {
+    console.log("GESTURE right !");
+    showMenuOnly();
+});
+
+if (toggleDescription !== null) {
+    var descriptionHammer = new Hammer(description);
+    var headerHammer = new Hammer(header);
+    
+    headerHammer.get('pan').set({ direction: Hammer.DIRECTION_ALL, threshold: 8 });
+    
+    descriptionHammer.on("panup", function(ev){ hideDescriptionOnly(); });
+    headerHammer.on("pandown", function(ev){ showDescriptionOnly(); });
+}
+
 //jQuery(window).scroll(top);
 /*jQuery(".to-top").click(function(e){
     e.preventDefault();
